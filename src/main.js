e39485d6861ee6e6eb03cb80edc516ecce83e192
SzybkiSzczypior <kalitar693@gmail.com>
2023-08-02 14:07:03 +0200

    --

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 const notesContainer = document.getElementById("app");
 const addNoteButton = notesContainer.querySelector(".add-note"); 
 
+getNotes().forEach(note => {
+    const noteElement = createNoteElement(note.id, note.content);
+    notesContainer.insertBefore(noteElement,addNoteButton);
+});
+
 function getNotes(){
 return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
 
@@ -12,6 +17,22 @@ function saveNotes(notes) {
 
 function createNoteElement(id, content) {
     const element = document.createElement("textarea");
+    element.classList.add("note");
+    element.value = content;
+    element.placeholder = "empty sticky note";
+
+    element.addEventListener("change",()=>{
+        updateNote(id, element.value);
+    });
+    element.addEventListener("dblclick", () =>{
+        const doDelete = confirm("are you sure about that?");
+
+        if (doDelete) {
+            deleteNote(id, element)
+        }
+    });
+
+    return element;
 }
 
 
@@ -20,9 +41,12 @@ function addNote() {
 }
 
 function updateNote(id,newContent){
+    console.log("updating note...");
+    console.log(id, newContent);
 
 }
 
 function deleteNote(id, element){
-
+    console.log("deleting note...");
+    console.log(id, newContent);
 }
