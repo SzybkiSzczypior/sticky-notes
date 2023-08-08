e57f06f76938d638ae43894f1ac15423c63820e5
SzybkiSzczypior <kalitar693@gmail.com>
2023-08-08 07:19:18 +0200

    --

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ getNotes().forEach(note => {
     notesContainer.insertBefore(noteElement,addNoteButton);
 });
 
+addNoteButton.addEventListener("click",() => addNote());
+
 function getNotes(){
 return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
 
@@ -19,7 +21,7 @@ function createNoteElement(id, content) {
     const element = document.createElement("textarea");
     element.classList.add("note");
     element.value = content;
-    element.placeholder = "empty sticky note";
+    element.placeholder = "empty sticky note"; 
 
     element.addEventListener("change",()=>{
         updateNote(id, element.value);
@@ -37,16 +39,31 @@ function createNoteElement(id, content) {
 
 
 function addNote() {
-    
+    const existingNotes = getNotes();
+    const noteObject = {
+        id: Math.floor(Math.random() * 100000),
+        content: ""
+    };
+
+    const noteElement = createNoteElement(noteObject.id, noteObject.content);
+    notesContainer.insertBefore(noteElement,addNoteButton);
+
+    existingNotes.push(noteObject);
+    saveNotes(existingNotes);
 }
 
 function updateNote(id,newContent){
-    console.log("updating note...");
-    console.log(id, newContent);
+const notes = getNotes();
+const targetNote = notes.filter(note => note.id == id)[0];
+
+targetNote.content = newContent;
+saveNotes(notes);
 
 }
 
 function deleteNote(id, element){
-    console.log("deleting note...");
-    console.log(id, newContent);
+ const notes = getNotes().filter(note => note.id != id);
+
+ saveNotes(notes);
+ notesContainer.removeChild(element);
 }
